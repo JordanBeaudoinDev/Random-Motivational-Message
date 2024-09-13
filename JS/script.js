ae2bb32cb3a6760940f346490761730c55768cda
Jordan Beaudoin <jordanbeaudoinpro@gmail.com>
2024-09-13 13:49:34 +0200

    First version the app

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,10 @@
-console.log('Bonjour');
+const quote = document.getElementById("quote");
+fetch('./js/quotes.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let num = Math.floor(Math.random() * 316);
+        let value = `<blockquote class="text"> <p>${data[num].quote}"</p><footer>- ${data[num].author}</footer></blockquote>`;
+        quote.innerHTML = value;
+    })
